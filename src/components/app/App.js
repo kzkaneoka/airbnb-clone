dba8938c60fb3f527174101e9fa60faff35f3b7a
kzkaneoka <kzkaneoka@gmail.com>
2020-12-13 10:01:22 +0900

    Refactored file structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './Header';
-import Home from './Home';
-import SearchPage from './SearchPage';
-import Footer from './Footer';
+import Header from '../header/Header';
+import Home from '../home/Home';
+import SearchPage from '../search/search-page/SearchPage';
+import Footer from '../footer/Footer';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Router>
         <Header />
         <Switch>
-          <Route path="/search">
+          <Route path='/search'>
             <SearchPage />
           </Route>
-          <Route path="/">
+          <Route path='/'>
             <Home />
           </Route>
         </Switch>
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,28 +2,28 @@ import React, { useState } from 'react';
 import './Banner.css';
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
-import Search from './Search';
+import Search from '../search/Search';
 
 function Banner() {
   const history = useHistory();
   const [showSearch, setShowSearch] = useState(false);
   return (
-    <div className="banner">
-      <div className="banner__search">
+    <div className='banner'>
+      <div className='banner__search'>
         {showSearch && <Search />}
         <Button
           onClick={() => setShowSearch(!showSearch)}
-          className="banner__searchButton"
-          variant="outlined" // https://material-ui.com/api/button/
+          className='banner__searchButton'
+          variant='outlined' // https://material-ui.com/api/button/
         >
           {showSearch ? 'Hide' : 'Search Dates'}
         </Button>
       </div>
-      <div className="banner__info">
+      <div className='banner__info'>
         <h1>Get out and stretch out your imagination</h1>
         <h5>
           Plan a different kind of getway to uncover the hidden gemes near you
-          <Button onClick={() => history.push('/search')} variant="outlined">
+          <Button onClick={() => history.push('/search')} variant='outlined'>
             Explore Nearby
           </Button>
         </h5>
