c1dad52df0090d0437447e315395185c6e8facd2
kzkaneoka <kzkaneoka@gmail.com>
2020-12-16 17:31:54 +0900

    Use api to fetch airbnb info instead of hard coding

--- src/components/search-result/SearchResult.js
+++ src/components/search-result/SearchResult.js
@@ -4,35 +4,35 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import StarIcon from '@material-ui/icons/Star';
 
 function SearchResult({
-  img,
-  location,
-  title,
-  description,
-  star,
-  price,
-  total,
+  airbnb: {
+    images,
+    isSuperhost,
+    kind,
+    lat,
+    lng,
+    name,
+    price,
+    rating,
+    reviewsCount,
+  },
 }) {
+  const img = images[0]['picture'];
   return (
-    <div className="searchResult">
-      <img src={img} alt="" />
-      <FavoriteBorderIcon className="searchResult__heart" />
-      <div className="searchResult__info">
-        <div className="searchResult__infoTop">
-          <p>{location}</p>
-          <h3>{title}</h3>
+    <div className='searchResult'>
+      <img src={img} alt='' />
+      <FavoriteBorderIcon className='searchResult__heart' />
+      <div className='searchResult__info'>
+        <div className='searchResult__infoTop'>
+          <h3>{name}</h3>
           <p>_____</p>
-          <p>{description}</p>
+          <p>some info here like number of guests, number of beds</p>
         </div>
-        <div className="searchResult__infoBottom">
-          <div className="searchResult__stars">
-            <StarIcon className="searchResult__star" />
+        <div className='searchResult__infoBottom'>
+          <div className='searchResult__stars'>
+            <StarIcon className='searchResult__star' />
             <p>
-              <strong>{star}</strong>
+              <strong>{rating}</strong> ({reviewsCount})
             </p>
-          </div>
-          <div className="searchResults__price">
-            <h2>{price}</h2>
-            <p>{total}</p>
           </div>
         </div>
       </div>
